b37e4117424b054b53c1936bcbdefc6c9e404f3e
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-31 10:17:27 -0300

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,25 +1,31 @@
-import fetch from 'node-fetch'
-
-var handler = async (m, { conn, usedPrefix, text, args, command }) => {
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => '')
-let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let bio = biografia.status?.toString() || 'Sin Biografía'
-let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
-
-await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('5217294888993'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
-], m)
+function handler(m) {
+let name = conn.getName('595992667005@s.whatsapp.net')
+let ownerN = '595992667005'
+conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+ contextInfo: { 
+ forwardingScore: 2023,
+isForwarded: false, 
+ externalAdReply: {  
+ title: `Este es mi maravilloso creador`, 
+ body: botName, 
+ sourceUrl: '',
+ thumbnail: imagen,
+ thumbnailUrl: '', 
+ mediaType: 1,
+ showAdAttribution: true, 
+ renderLargerThumbnail: true 
+ }
+   }
+     },
+       {
+         quoted: fkontak
+           }
+             );
 
 }
-handler.help = ['owner', 'contacto', 'creador', 'contactos']
-handler.tags = ['info']
-handler.command = /^(owner|contacto|creador|contactos)/i
 
-handler.register = true
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño', 'fgowner'] 
 
 export default handler
