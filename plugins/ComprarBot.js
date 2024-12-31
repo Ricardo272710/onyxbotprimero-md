5de848bdd051ea1f0ad0d251f25909328358ccd2
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-31 11:29:29 -0300

    Fn-Bot

---
+++ plugins/ComprarBot.js
@@ -0,0 +1,15 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.ComprarBot);
+};
+handler.command = /^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
+export default handler;
+
+global.ComprarBot = `
+〔 Spider 𝗕𝗼𝘁 🐉 〕
+
+HABLAR AL :
+wa.me/593979133620
+
+HABLAR AL :
+wa.me/50764206082
+`;
