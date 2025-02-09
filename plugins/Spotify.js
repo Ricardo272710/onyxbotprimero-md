4cdeedab3bee89df8f91d00edbf0e14edab7fe00
MatiasC-15 <kenddrysilva15@gmail.com>
2025-02-08 23:45:37 -0500

    Update Spotify.js

--- plugins/Spotify.js
+++ plugins/Spotify.js
@@ -1,25 +1,76 @@
-/* 
-- Downloader Spotify By Izumi-kzx
+/*
+## Spotify downloads with buttons: URL and text 
+> I'm Fz && LyraX
 - https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
 */
+const key = '' // Obtén tu apikey registrandote aquí: https://api.lyrax.net
 
-// *[🍧 SPOTIFY DOWNLOADER]*
-import fetch from 'node-fetch';
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+if (!text) throw `🌱 Ingrese un texto o URL lara buscar o descargar una canción de Spotify.`
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingresa el nombre o enlace de *Spotify*.', m);
+if (command === 'spotify' || command === 'spotifys') {
+let texto = text
+m.react(rwait)
 try {
-let res = await fetch(`https://api.vreden.web.id/api/spotify?url=${encodeURIComponent(text)}`);
-let json = await res.json();
-if (json.status === 200 && json.result?.status) {
-let { title, artists, cover, music } = json.result;
-let msg = `🎵 *Título*: ${title}\n🎤 *Artista*: ${artists}\n📅 *Lanzamiento*: ${json.result.releaseDate}`;
-await conn.sendFile(m.chat, cover, 'cover.jpg', msg, m);
-await conn.sendMessage(m.chat, { audio: { url: music }, fileName: `${title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
-} else conn.reply(m.chat, '🚩 No se pudo obtener la música.', m);
-} catch { conn.reply(m.chat, '🚩 Error al procesar la solicitud.', m); }
-};
+let res = await (await fetch(`https://api.lyrax.net/api/search/spotify-s?text=${texto}&apikey=${key}`)).json()
 
-handler.command = /^(spotify|sp)$/i;
+let { title, popularity, url } = res.result[0]
 
+let info = await (await fetch(`https://api.lyrax.net/api/dl/spotifyV2?url=${url}&apikey=${key}`)).json()
+let { img, duration } = info.data
+let txt = `
+╌╌╌⟢ \`Spotify | Download\` ⟣╌╌╌
+ 
+ 📦 \`Titulo\` : ${title}
+ 🗓️ \`Duración\` : ${duration}
+ 🏆 \`Popularidad\` : ${popularity}
+ 🖇️ \`URL\` : ${url}
+`;
+await conn.sendMessage(m.chat, {
+      image: { url: img },
+      caption: txt,
+      footer: `© Sylphiette | Powered by I'm Fz ~`,
+      buttons: [
+        {
+          buttonId: `.spotifydl ${url}`,
+          buttonText: {
+            displayText: '🎵 Descargar Canción',
+          },
+        },
+      ],
+      viewOnce: true,
+      headerType: 4,
+    }, { quoted: m });
+    m.react('🌱');
+    } catch {
+    throw `Ocurrió un error. Intente usando el nombre específico del artista o canción tal cual está en Spotify.`
+    m.react(error)
+    }
+} else if (command === 'spotifydl') {
+let url = text
+let isValid = /^https?:\/\/(open|play)\.spotify\.com\/(track|album|artist|playlist)\/[a-zA-Z0-9]+$/i.test(url)
+if (isValid) {
+m.react(rwait);
+try {
+let dll = await (await fetch(`https://api.lyrax.net/api/dl/spotifyV2?url=${url}&apikey=${key}`)).json()
+let { dl, song_name, artist } = dll.data
+conn.sendMessage(m.chat, { audio: { url: dl }, mimetype: "audio/mpeg" }, { quoted: m})
+m.react(done);
+} catch {
+throw `Ocurrió un error. Intente con otra URL.`
+m.react(error);
+}
+} else {
+throw `Ingresa una URL válida de Spotify. Ejemplo:
+## https://open.spotify.com/track/6tMdpUXfT70TD6Eh3XfB1p
+`
+m.react(error)
+}
+} else {
+throw `Comando no reconocido.`
+}
+}
+handler.tags = ['dl'];
+handler.help = ['spotify'];
+handler.command = ['spotify', 'spotifys', 'spotifydl'];
 export default handler;
