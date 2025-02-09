b380ef7e37571cdb6752bfe0e88b009170b2db35
MatiasC-15 <kenddrysilva15@gmail.com>
2025-02-09 00:20:46 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,9 +1,25 @@
-//By JTxs  Ivanmods15
+/* 
+
+*❀ By JTxs*
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
 // *[ ❀ PLAY ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `Ingresa el nombre de la cancion que quieras buscar`, m)
+if (!text) return conn.reply(m.chat, `❀ Ingresa el nombre de la cancion que quieras buscar`, m)
 
 try {
 let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${text}`)
