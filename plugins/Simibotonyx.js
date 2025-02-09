f00660047e8dc121c10ea78b5accba2ca632840d
MatiasC-15 <kenddrysilva15@gmail.com>
2025-02-08 23:52:10 -0500

    Update Simibotonyx.js

--- plugins/Simibotonyx.js
+++ plugins/Simibotonyx.js
@@ -3,20 +3,22 @@
 - Power By Team Code Titans
 - https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
 */
-// *[ 🍟 LORI AI ]*
+// *[ 🍨 LILYCHAN AI ]*
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `🍟 Ingresa un texto para hablar con Lori Ai`, m)
-try {
-let api = await fetch(`https://api.davidcyriltech.my.id/ai/lori?text=${encodeURIComponent(text)}`)
-let json = await api.json()
-if (json.success) await m.reply(json.response)
-else await m.reply(`🍟 Hubo un error al obtener la respuesta.`)
-} catch (error) {
+let handler=async(m,{conn,text})=>{
+if(!text)return conn.reply(m.chat,`🌸 Ingresa un texto para hablar con LilyChan`,m)
+try{
+let api=await fetch(`https://archive-ui.tanakadomp.biz.id/ai/lilychan?text=${encodeURIComponent(text)}`)
+let json=await api.json()
+if(json.status&&json.result)await m.reply(json.result.message)
+else await m.reply(`🌸 Hubo un error al obtener la respuesta.`)
+}catch(error){
 console.error(error)
-await m.reply(`🍟 Ocurrió un error al procesar tu solicitud.`)
+await m.reply(`🌸 Ocurrió un error al procesar tu solicitud.`)
 }}
-handler.command = ['loriai', 'lori'];
+handler.help=['lilychan *<texto>*']
+handler.tags=['ai']
+handler.command=['onyx']
 
 export default handler
