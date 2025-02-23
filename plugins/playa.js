ab753fc78860dfa9cddb1ec5810f5acc9a9601c6
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2025-02-23 16:21:49 -0500

    Update playa.js

--- plugins/playa.js
+++ plugins/playa.js
@@ -1,34 +1,20 @@
-/* 
-- Código Creado Por Izumi-kzx
-- Power By Team Code Titans
-- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S
-*/
-// [ 🍟 SOUND CLOUD SEARCH ]
-import fetch from 'node-fetch'
+const fetch = require("node-fetch")
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args.length) return conn.reply(m.chat, 🔍 *Por favor escribe una canción a buscar.*\nEjemplo: ${usedPrefix}${command} Avión Privado, m)
-  const query = args.join(' ')
-  try {
-    const response = await fetch(https://api.agungny.my.id/api/soundclouds?q=${encodeURIComponent(query)})
-    const data = await response.json()
-    
-    if (!data.status || !data.result || data.result.length === 0) {
-      return conn.reply(m.chat, 🚫 No se encontraron resultados para "${query}"., m)
-    }
-    
-    let txt = '🎵 S E A R C H - S O U N D C L O U D\n\n'
-    data.result.forEach(track => {
-      txt += 🎶 *Título*: ${track.title}\n
-      txt += 🔗 *Enlace*: ${track.url}\n\n---------------------------------------------------\n\n
-    })
-    
-    await conn.reply(m.chat, txt.trim(), m)
-  } catch (error) {
-    console.error(error)
-    conn.reply(m.chat, '❌ Hubo un error al procesar la solicitud.', m)
-  }
+case 'sps':
+case 'spotifys':
+case 'spotifysearch':
+case 'spotify': {
+if (!text) return m.reply(${prefix + command} untuk mencintaimu)
+m.reply('Proses 🗿')
+await fetch(https://api.diioffc.web.id/api/search/spotify?query=${text}).then(async (res) => {
+let response = await res.json()
+let teks = '🔎 Hasil Pencarian SPOTIFY\n\n'
+for (let i of response.result) {
+teks += *◦ Judul :* ${i.trackName}\n
+teks += *◦ Artis :* ${i.artistName}\n
+teks += *◦ Link Url :* ${i.externalUrl}\n\n
 }
-handler.command = ['sound']
-
-export default handler
+m.reply(teks)
+}).catch(err => m.reply('Error 🗿'))
+}
+break
